836b7c83602d91a7cbb794b5d59b1fe97dc63581
Kevin <ketsang12@gmail.com>
2018-12-17 12:03:37 -0500

    initial rough commit

--- config.js
+++ config.js
@@ -2,13 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
-    TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
-    // DATABASE_URL:
-    //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
+     'mongodb://dev:yesmedev1@ds125001.mlab.com:25001/web-hook',
     // TEST_DATABASE_URL:
     //     process.env.TEST_DATABASE_URL ||
-    //     'postgres://localhost/thinkful-backend-test'
+    //     'mongodb://localhost/thinkful-backend-test'
 };
